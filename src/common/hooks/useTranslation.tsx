836a0a7e819fe0e6381987ee1379fb78d00c281a
vustinchdev <vustinchdev@gmail.com>
2025-04-03 14:31:17 +0300

    INC-189 feat: add LangSelect component for language selection

--- src/common/hooks/useTranslation.tsx
+++ src/common/hooks/useTranslation.tsx
@@ -1,8 +1,12 @@
+import { usePathname } from 'next/navigation'
+
 import { en } from '../../../locales/en'
 import { ru } from '../../../locales/ru'
 
 export const useTranslation = () => {
-  const locale = 'en'
+  const pathname = usePathname()
+  const segments = pathname.split('/')
+  const locale = segments[1]
 
   const t = locale === 'en' ? en : ru
 
