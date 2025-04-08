ccc2d26faecd6a6dba122ef75337749a7b1a4fe7
vustinchdev <vustinchdev@gmail.com>
2025-04-08 12:15:11 +0300

    INC-192 feat: add useQueryParams

---
+++ src/common/hooks/useQueryParams.ts
@@ -0,0 +1,30 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+export const useQueryParams = () => {
+  const { push } = useRouter()
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+
+  const setQueryParams = (params: Record<string, string>) => {
+    const urlSearchParams = new URLSearchParams(searchParams.toString())
+
+    Object.keys(params).forEach(key => {
+      urlSearchParams.set(key, params[key])
+    })
+    push(`${pathname}?${urlSearchParams.toString()}`)
+  }
+
+  const resetParams = () => {
+    const urlSearchParams = new URLSearchParams()
+
+    push(`${pathname}?${urlSearchParams.toString()}`)
+  }
+
+  return {
+    resetParams,
+    searchParams,
+    setQueryParams,
+  }
+}
