29b51734e221ec29163f628b21f91c784e4f251e
vustinchdev <vustinchdev@gmail.com>
2025-04-08 15:09:08 +0300

    INC-192 feat: add get payments query by user

--- src/services/userService.generated.tsx
+++ src/services/userService.generated.tsx
@@ -43,6 +43,34 @@ export type GetPostsByUserQuery = {
   }
 }
 
+export type GetPaymentsByUserQueryVariables = Types.Exact<{
+  userId: Types.Scalars['Int']['input']
+  pageSize?: Types.InputMaybe<Types.Scalars['Int']['input']>
+  pageNumber?: Types.InputMaybe<Types.Scalars['Int']['input']>
+  sortBy?: Types.InputMaybe<Types.Scalars['String']['input']>
+  sortDirection?: Types.InputMaybe<Types.SortDirection>
+}>
+
+export type GetPaymentsByUserQuery = {
+  __typename?: 'Query'
+  getPaymentsByUser: {
+    __typename?: 'PaymentPaginationModel'
+    pagesCount: number
+    page: number
+    pageSize: number
+    totalCount: number
+    items: Array<{
+      __typename?: 'SubscriptionByPaymentModel'
+      id: string
+      dateOfPayment?: any | null
+      endDate?: any | null
+      price: number
+      type: Types.SubscriptionType
+      paymentType?: Types.PaymentMethod | null
+    }>
+  }
+}
+
 export const GetUserDocument = gql`
   query GetUser($userId: Int!) {
     getUser(userId: $userId) {
@@ -177,4 +205,98 @@ export type GetPostsByUserSuspenseQueryHookResult = ReturnType<
 export type GetPostsByUserQueryResult = Apollo.QueryResult<
   GetPostsByUserQuery,
   GetPostsByUserQueryVariables
+>
+export const GetPaymentsByUserDocument = gql`
+  query GetPaymentsByUser(
+    $userId: Int!
+    $pageSize: Int
+    $pageNumber: Int
+    $sortBy: String
+    $sortDirection: SortDirection
+  ) {
+    getPaymentsByUser(
+      userId: $userId
+      pageSize: $pageSize
+      pageNumber: $pageNumber
+      sortBy: $sortBy
+      sortDirection: $sortDirection
+    ) {
+      pagesCount
+      page
+      pageSize
+      totalCount
+      items {
+        id
+        dateOfPayment
+        endDate
+        price
+        type
+        paymentType
+      }
+    }
+  }
+`
+
+/**
+ * __useGetPaymentsByUserQuery__
+ *
+ * To run a query within a React component, call `useGetPaymentsByUserQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetPaymentsByUserQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetPaymentsByUserQuery({
+ *   variables: {
+ *      userId: // value for 'userId'
+ *      pageSize: // value for 'pageSize'
+ *      pageNumber: // value for 'pageNumber'
+ *      sortBy: // value for 'sortBy'
+ *      sortDirection: // value for 'sortDirection'
+ *   },
+ * });
+ */
+export function useGetPaymentsByUserQuery(
+  baseOptions: Apollo.QueryHookOptions<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables> &
+    ({ variables: GetPaymentsByUserQueryVariables; skip?: boolean } | { skip: boolean })
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+
+  return Apollo.useQuery<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables>(
+    GetPaymentsByUserDocument,
+    options
+  )
+}
+export function useGetPaymentsByUserLazyQuery(
+  baseOptions?: Apollo.LazyQueryHookOptions<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables>
+) {
+  const options = { ...defaultOptions, ...baseOptions }
+
+  return Apollo.useLazyQuery<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables>(
+    GetPaymentsByUserDocument,
+    options
+  )
+}
+export function useGetPaymentsByUserSuspenseQuery(
+  baseOptions?:
+    | Apollo.SkipToken
+    | Apollo.SuspenseQueryHookOptions<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables>
+) {
+  const options =
+    baseOptions === Apollo.skipToken ? baseOptions : { ...defaultOptions, ...baseOptions }
+
+  return Apollo.useSuspenseQuery<GetPaymentsByUserQuery, GetPaymentsByUserQueryVariables>(
+    GetPaymentsByUserDocument,
+    options
+  )
+}
+export type GetPaymentsByUserQueryHookResult = ReturnType<typeof useGetPaymentsByUserQuery>
+export type GetPaymentsByUserLazyQueryHookResult = ReturnType<typeof useGetPaymentsByUserLazyQuery>
+export type GetPaymentsByUserSuspenseQueryHookResult = ReturnType<
+  typeof useGetPaymentsByUserSuspenseQuery
+>
+export type GetPaymentsByUserQueryResult = Apollo.QueryResult<
+  GetPaymentsByUserQuery,
+  GetPaymentsByUserQueryVariables
 >
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -30,3 +30,22 @@ export const GET_POSTS_BY_USER = gql(`
     } 
   }
 `)
+
+export const GET_PAYMENTS_BY_USER = gql(`
+  query GetPaymentsByUser($userId: Int!, $pageSize: Int, $pageNumber: Int, $sortBy: String, $sortDirection: SortDirection) {
+    getPaymentsByUser(userId: $userId, pageSize: $pageSize, pageNumber: $pageNumber, sortBy: $sortBy, sortDirection: $sortDirection) {
+      pagesCount
+      page
+      pageSize
+      totalCount
+      items {
+        id
+        dateOfPayment
+        endDate
+        price
+        type
+        paymentType
+      }
+    } 
+  }
+`)
