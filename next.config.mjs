ee8530ae3631c639d35cda2e77ee050400289895
vustinchdev <vustinchdev@gmail.com>
2025-04-08 14:59:14 +0300

    INC-192 feat: configure next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['staging-it-incubator.s3.eu-central-1.amazonaws.com'],
+        remotePatterns: [
+            {
+            hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+            pathname: '/trainee-instagram-api/**',
+            port: '',
+            protocol: 'https',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
