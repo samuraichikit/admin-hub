074cc562a60d95034451d80065af1e8b4fa2dbc1
vustinchdev <vustinchdev@gmail.com>
2025-05-17 11:31:26 +0300

    INC-219 feat: add sorting functionality to PaymentsList

--- src/app/[locale]/(dashboard)/paymentsList/_components/paymentsList/PaymentsList.tsx
+++ src/app/[locale]/(dashboard)/paymentsList/_components/paymentsList/PaymentsList.tsx
@@ -1,28 +1,32 @@
 'use client'
 
 import { DEFAULT_PAGE_NUMBER, PAYMENTS_LIST_PAGE_SIZE } from '@/common/constants'
-import { useCommonTablePagination } from '@/common/hooks'
+import { useCommonTablePagination, useSort } from '@/common/hooks'
 import { Column, CommonTableWithPagination } from '@/components/ui'
 import { useGetPaymentsQuery } from '@/services/paymentsService.generated'
-import { SubscriptionPaymentsModel } from '@/services/types'
+import { SortDirection, SubscriptionPaymentsModel } from '@/services/types'
 
 import { AmountRow } from '../amountRow'
 import { UserRow } from '../userRow'
 
+type PaymentColumn = Column<SubscriptionPaymentsModel>
+type PaymentColumnAccessor = PaymentColumn['accessor']
+
 export const PaymentsList = () => {
-  const columns: Column<SubscriptionPaymentsModel>[] = [
+  const columns: PaymentColumn[] = [
     {
       accessor: 'userName',
       sortable: true,
       title: 'Username',
       Cell: row => <UserRow src={row.avatars?.[0]?.url ?? ''} userName={row.userName} />,
     },
-    { accessor: 'createdAt', title: 'Date added' },
+    { accessor: 'createdAt', title: 'Date added', sortable: true },
 
     {
       accessor: 'amount',
       title: `Amount, $`,
       Cell: row => <AmountRow amount={row.amount ?? 0} currency={row.currency ?? ''} />,
+      sortable: true,
     },
     { accessor: 'type', title: 'Subscription' },
     { accessor: 'paymentMethod', sortable: true, title: 'Payment Method' },
@@ -32,7 +36,13 @@ export const PaymentsList = () => {
       defaultPageNumber: DEFAULT_PAGE_NUMBER,
       defaultPageSize: PAYMENTS_LIST_PAGE_SIZE,
     })
-  const { data } = useGetPaymentsQuery({ variables: { pageNumber, pageSize } })
+  const { handleChangeSort, sortBy, sortDirection } = useSort<PaymentColumnAccessor>({
+    defaultSortBy: 'createdAt',
+    defaultSortDirection: SortDirection.Desc,
+  })
+  const { data } = useGetPaymentsQuery({
+    variables: { pageNumber, pageSize, sortBy, sortDirection },
+  })
 
   const paymentsData = data?.getPayments.items ?? []
   const perPageOptions = [6, 10, 20, 50, 100]
@@ -43,12 +53,15 @@ export const PaymentsList = () => {
         columns={columns}
         currentPage={pageNumber}
         onPageChange={handleChangeCurrentPage}
+        onChangeSort={handleChangeSort}
         tableBodyData={paymentsData}
         totalCount={100}
         pageSize={pageSize}
         onPageSizeChange={handlePageSizeChange}
         showPerPageSelect
         perPageOptions={perPageOptions}
+        sortColumn={sortBy}
+        sortDirection={sortDirection}
       />
     </>
   )
