31f706d11ccf4007cdd046549d008c7298dd3d90
vustinchdev <vustinchdev@gmail.com>
2025-05-22 15:07:03 +0300

    INC-219 feat: use relative layout for images in UserUploadedPhotos

--- src/app/[locale]/(dashboard)/usersList/[id]/_components/userUploadedPhotos/UserUploadedPhotosSkeleton.tsx
+++ src/app/[locale]/(dashboard)/usersList/[id]/_components/userUploadedPhotos/UserUploadedPhotosSkeleton.tsx
@@ -2,14 +2,22 @@
 
 import Skeleton, { SkeletonProps } from 'react-loading-skeleton'
 
+import s from './userUploadedPhotos.module.scss'
+
 export const UploadedPhotosSkeleton = ({ count, ...rest }: SkeletonProps) => {
+  const classNames = {
+    container: s.container,
+    imageWrapper: s.imageWrapper,
+    skeletonItem: s.skeletonItem,
+  }
+
   return (
-    <>
+    <div className={classNames.container}>
       {Array.from({ length: count ?? 1 }).map((_, index) => (
-        <div key={index}>
-          <Skeleton {...rest} />
+        <div key={index} className={classNames.imageWrapper}>
+          <Skeleton {...rest} className={classNames.skeletonItem} />
         </div>
       ))}
-    </>
+    </div>
   )
 }
